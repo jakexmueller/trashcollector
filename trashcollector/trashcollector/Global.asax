e5d51c7ea8e92d084b9cf559d0bc32db895177ad
Jacob <jakexmueller@gmail.com>
2018-07-05 10:50:56 -0500

    adding new files

---
+++ trashcollector/trashcollector/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="trashcollector.MvcApplication" Language="C#" %>
---
+++ trashcollector/trashcollector/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(trashcollector.Startup))]
+namespace trashcollector
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
