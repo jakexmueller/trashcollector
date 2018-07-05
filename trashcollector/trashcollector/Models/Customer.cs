4e1f58bd19095b92fb772b72fdaf65fa3d735e05
Jacob <jakexmueller@gmail.com>
2018-07-05 10:55:49 -0500

    added customer class model

---
+++ trashcollector/trashcollector/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace trashcollector.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int ZipCode { get; set; }
+        public string PickupDay { get; set;}
+
+    }
+}
